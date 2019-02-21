89e0b31701b5057327265d5518d27f9cbeefb5ac
mak20th <mak20th@gmail.com>
2019-02-21 16:28:40 -0600

    added problem1-5 from object drills

--- index.js
+++ index.js
@@ -65,4 +65,112 @@ function validateKeys(object, expectedKeys) {
   }
   return true;  
 }  
+//===================================================================================================
 
+// const loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function(){
+//     return ((this.water/this.flour)*100);
+//   }
+  
+// };
+// console.log(loaf.flour);
+// console.log(loaf.water);
+// console.log(loaf.hydration());
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// const myObj2 = {
+//   foo: 'foo2',
+//   bar: 'bar2',
+//   fum: 'fum2',
+//   quux: 'quux2',
+//   spam: 'spam2',
+// };
+
+// for(let property in myObj2){
+//   console.log(property+ ': ' + myObj2[property]);
+// }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// const myObj3 = {
+//   meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'],
+// };
+
+// console.log(myObj3.meals[3]);
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// const myObj4 = {
+//   name: 'Dwight',
+//   jobTitle: 'assistant to the regional manager',
+// };
+
+// const myObj5 = {
+//   name: 'Jim',
+//   jobTitle: 'Regional manager',
+// };
+
+// const myObj6 = {
+//   name: 'Michael',
+//   jobTitle: 'Senior regional manager',
+// };
+
+// const myArray =[myObj4, myObj5, myObj6];
+
+// for(let x in myArray){
+//   console.log(myArray[x].name + ': ' + myArray[x].jobTitle);
+// }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+
+// const myObj4 = {
+//   name: 'Dwight',
+//   jobTitle: 'assistant to the regional manager',
+//   boss: 'Jim',
+// };
+
+// const myObj5 = {
+//   name: 'Jim',
+//   jobTitle: 'Regional manager',
+//   boss: 'Michael',
+// };
+
+// const myObj6 = {
+//   name: 'Michael',
+//   jobTitle: 'Senior regional manager',
+// };
+
+// const myArray =[myObj4, myObj5, myObj6];
+
+// for(let x in myArray){
+//   if(myArray[x].hasOwnProperty('boss')){
+//     console.log(`${myArray[x].jobTitle} ${myArray[x].name} reports to ${myArray[x].boss}`);
+//   }
+//   else{
+//     console.log(`${myArray[x].jobTitle} ${myArray[x].name} doesn't report to anybody.`);
+//   }  
+// }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const cipher ={
+  a: 2,
+  b: 3,
+  c: 4,
+  d: 5,  
+};
+
+function decode(encodedWord) {
+    switch(encodedWord.substring(0,1)) {
+    case 'a':
+      return encodedWord.substring(1,2);
+    case 'b':
+      return encodedWord.substring(2,3);
+    case 'c':
+      return encodedWord.substring(3,4);
+    case 'd':
+      return encodedWord.substring(4,5);
+    default:
+      return ' ';
+    }
+  }
